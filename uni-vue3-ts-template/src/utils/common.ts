e26e85e5967a10eb06aac81000e40e108a593085
windyeasy <windyeasy13gb@163.com>
2024-10-19 10:47:04 +0800

    feat: 添加常用方法

--- uni-vue3-ts-template/src/utils/common.ts
+++ uni-vue3-ts-template/src/utils/common.ts
@@ -1,21 +1,23 @@
 // 友情提示
-export function showFriendlyTips(msg) {
+export function showFriendlyTips(msg: string) {
   return new Promise((resolve, reject) => {
     uni.showModal({
-      title: "友情提示",
+      title: '友情提示',
       content: msg,
       showCancel: false,
       success: resolve,
       fail: reject,
-    });
-  });
+    })
+  })
 }
-
 
 /**
  *对showToast 封装
  */
- export function showToast(title, options={}) {
+export function showToast(
+  title: string,
+  options: UniApp.ShowToastOptions = {},
+) {
   return new Promise((resolve, reject) => {
     uni.showToast({
       title,
@@ -32,22 +34,23 @@ export function showFriendlyTips(msg) {
   })
 }
 
-
 // 打电话
-export function callPhone(phone) {
+export function callPhone(phone: string) {
   uni.makePhoneCall({
     phoneNumber: phone,
   })
 }
+
 // 字符串到数组
-export function strToArr(str, symbol = ',') {
+export function strToArr(str: string, symbol = ',') {
   if (typeof str === 'string') {
     return str.split(symbol)
   }
   return []
 }
+
 // 数组到字符串
-export function arrToStr(arr, symbol = ',') {
+export function arrToStr(arr: Array<string | number>, symbol = ',') {
   if (Array.isArray(arr)) {
     return arr.join(symbol)
   }
