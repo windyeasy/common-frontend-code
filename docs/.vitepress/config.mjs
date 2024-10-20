f28e3126bbee09e4f5184bd16295451a423c7ce2
windyeasy <windyeasy13gb@163.com>
2024-10-20 12:12:42 +0800

    feat: 添加网站图标

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "common-frontend-code",
   description: "前端常用代码库",
+  head: [
+    ['link', { rel: 'icon', type: 'image/svg', href: '/logo.svg' }]
+  ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
